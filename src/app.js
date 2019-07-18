3a0f5d3c5fe660015c3ee5d20c2b450b4ed970d6
Sam Aripov <aripov85cm@gmail.com>
2019-07-17 22:42:14 -0400

    Init Commit

--- src/app.js
+++ src/app.js
@@ -3,13 +3,9 @@ import ReactDOM from 'react-dom'
 import { Provider } from 'react-redux'
 import MainRouter from './routers/MainRouter'
 import configureStore from './store/configureStore'
-import { addExpense, removeExpense, editExpense } from './actions/expenses'
-import { setTextFilter, sortByDate, sortByAmount, setStartDate, setEndDate } from './actions/filters'
-import getFilteredItems from './selectors/expenses'
 import 'normalize.css/normalize.css'
 import './styles/styles.scss'
 import "react-datepicker/dist/react-datepicker.css"
-
 
 const store = configureStore()
 
