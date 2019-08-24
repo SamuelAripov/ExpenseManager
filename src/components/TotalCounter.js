1c08aff150b1074b2b750107c5213218a67d585b
Sam Aripov <aripov85cm@gmail.com>
2019-08-24 17:58:27 -0400

    Fixed 'Total Counter'

--- src/components/TotalCounter.js
+++ src/components/TotalCounter.js
@@ -5,23 +5,21 @@ import numeral from 'numeral'
 export class TotalCounter extends React.Component {
     constructor(props) {
         super(props)
-        this.state = {
-            sum: 0
-        }
+    }
+    sum() {
+        let sum = 0
         if (this.props.expenses) {
-            let sum = 0
             this.props.expenses.map((expense) => {
                 sum += parseFloat(expense.amount)
+                console.log(expense)
             })
-            this.state = {
-                sum
-            }
         }
+        return sum
     }
     render() {
         return (
             <div>
-                {this.props.expenses ? <h1>Viewing {this.props.expenses.length} expense(s) with the total of {numeral(this.state.sum).format('$0,0.00')}</h1> : <h1></h1>}
+                {this.props.expenses ? <h1>Viewing {this.props.expenses.length} expense(s) with the total of {numeral(this.sum()).format('$0,0.00')}</h1> : <h1></h1>}
             </div>
         )
     }
